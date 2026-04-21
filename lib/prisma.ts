bb33d8e3aa8f9482c442c765d938012e7364a57d
unknown <rrassya404@gmail.com>
2026-04-21 11:42:02 +0700

    fix: add prisma generate to build script

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import mysql from "mysql2/promise"; // Pastikan sudah install mysql2
 
