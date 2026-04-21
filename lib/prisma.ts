b83197afc09985e042bb0fcc58d293d0db0ebea4
unknown <rrassya404@gmail.com>
2026-04-21 11:48:53 +0700

    fix: resolve prisma adapter type rror for deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import mysql from "mysql2/promise"; // Pastikan sudah install mysql2
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
 const createPrismaClient = () => {
-  // 1. Buat pool koneksi mysql2 secara manual agar lebih stabil
-  const pool = mysql.createPool(process.env.DATABASE_URL!);
+  // 1. Inisialisasi adapter langsung dengan URL database (Prisma 7)
+  const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
   
-  // 2. Bungkus ke dalam adapter Prisma
-  const adapter = new PrismaMariaDb(pool);
-
-  // 3. Return client dengan adapter tersebut
+  // 2. Return client dengan adapter tersebut
   return new PrismaClient({ adapter });
 };
+
 
 export const prisma = globalForPrisma.prisma || createPrismaClient();
 
